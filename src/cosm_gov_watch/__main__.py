472d6e8bb2f71149e1fc13cc3da705980390da00
qf3l3k <36550920+qf3l3k@users.noreply.github.com>
2022-11-08 12:58:41 +0100

    Add files via upload
    Initial commit to repository.

---
+++ src/cosm_gov_watch/__main__.py
@@ -0,0 +1,33 @@
+from cosm_gov_watch.api_requests import request_governance
+from config_loader.load_config import load_chains
+from prettytable import PrettyTable
+
+proposals_table = PrettyTable()
+proposals_table.title = f"Chain Proposals"
+proposals_table.field_names = ["Chain", "ID", "Type", "Title", "Status"]
+proposals_table.align = "l"
+proposals_table.border = True
+
+
+def main():
+    chains = load_chains()
+    for chain in chains["chains"]:
+        proposals_table.add_row([chain["name"],
+                                 "--",
+                                 "--",
+                                 "--",
+                                 "--"])
+        governance = request_governance(chain["api"])
+        for proposal in governance['proposals']:
+            if proposal['status'] == "PROPOSAL_STATUS_VOTING_PERIOD":
+                proposals_table.add_row([chain["name"],
+                                         proposal['proposal_id'],
+                                         proposal['content']['@type'].rsplit('.', 1)[-1],
+                                         proposal['content']['title'][:60],
+                                         proposal['status']])
+
+    print(proposals_table)
+
+
+if __name__ == '__main__':
+    main()
