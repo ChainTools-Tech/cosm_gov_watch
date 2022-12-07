d9e4d7056295e85030405bc99c5e5849e7284511
qf3l3k <36550920+qf3l3k@users.noreply.github.com>
2022-12-07 16:12:53 +0100

    Update __main__.py

--- src/cosm_gov_watch/__main__.py
+++ src/cosm_gov_watch/__main__.py
@@ -17,7 +17,7 @@ def main():
                                  "--",
                                  "--",
                                  "--"])
-        print(f'\rProcessing: {chain["name"]} -> calling API: {chain["api"]}', end = "\x1b[1K\r")
+        print(f'\rProcessing: {chain["name"]} -> calling API: {chain["api"]}', " "*40, end = "")
         governance = request_governance(chain["api"])
         try:
             for proposal in governance['proposals']:
